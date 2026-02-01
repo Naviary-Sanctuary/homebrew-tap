474e30449479446182a1f79e51fb3c86a78d4a21
changchanghwang <hch950627@naver.com>
2026-02-01 21:01:11 +0900

    chore: bump to v0.1.1

--- Formula/schema-gen.rb
+++ Formula/schema-gen.rb
@@ -2,17 +2,17 @@ class SchemaGen < Formula
   desc "Automatically generate validation schemas from TypeScript class definitions"
   homepage "https://github.com/Naviary-Sanctuary/schema-gen"
   license "MIT"
-  version "v0.1.0"
+  version "v0.1.1"
 
   on_macos do
-    url "https://github.com/Naviary-Sanctuary/schema-gen/releases/download/v0.1.0/schema-gen-macos-arm64"
-    sha256 "c627c0df124dc35aead6eb46b8070b872d0ab76e46b80487d2c8f310be5ab69b"
+    url "https://github.com/Naviary-Sanctuary/schema-gen/releases/download/v0.1.1/schema-gen-macos-arm64"
+    sha256 "cfda0bbeb271ead4d52ccd3d6decefc134afc07112f625679c9674fa76867a46"
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Naviary-Sanctuary/schema-gen/releases/download/v0.1.0/schema-gen-linux-x64"
-      sha256 "5d06a21ca4705834416fb2977efd9c701953072d1d95c72f8f352fb45b0f8e2d"
+      url "https://github.com/Naviary-Sanctuary/schema-gen/releases/download/v0.1.1/schema-gen-linux-x64"
+      sha256 "a9cad7d1eb67e4885f38e3e25b02defbaf358cc56da8a91fa7b06c59596632dc"
     end
   end
 
